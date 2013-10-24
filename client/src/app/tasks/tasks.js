bb52cfa40cde200845eeca728adc66e195ba81d2
김종록 <jrkim123us@gmail.com>
2013-10-24 17:58:16 +0900

    서버단이 아닌 화면단에서 wbs 기준으로 sorting 함.

--- client/src/app/tasks/tasks.js
+++ client/src/app/tasks/tasks.js
@@ -21,6 +21,18 @@ angular.module('tasks', ['services.crud'])
 			$scope.breadcrumbList = getParentsWbs(result.task.wbs);
 		});
 	}
+	// wbs 기준으로 sorting 하기
+	// angular 제공하는 sort는 compare 방식이 아니라서
+	// 각 level 별 제곱수를 더하여 sorting 함
+	$scope.sortWbs = function(task) {
+		var wbs = task.wbs,
+ 			sortNum = 0;
+
+ 		angular.forEach(wbs.split('.'), function(value, inx){
+ 			sortNum = value * Math.pow(100, inx);
+ 		});
+		return sortNum;
+	}
 
 	function getParentsWbs(currentWbs) {
 		var results = [],
