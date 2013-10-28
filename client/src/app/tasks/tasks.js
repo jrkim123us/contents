c9d9d8e96e7f9d72280c926e9498c825dcf824d0
김종록 <jrkim123us@gmail.com>
2013-10-28 18:10:21 +0900

    1.jquery-ui sortable 기능 TASK 에 추가 2. 관련 components 추가

--- client/src/app/tasks/tasks.js
+++ client/src/app/tasks/tasks.js
@@ -1,14 +1,53 @@
-angular.module('tasks', ['services.crud'])
-
+angular.module('tasks', [
+	'services.crud',
+	'ui.sortable'
+])
 .config(['$routeProvider', function ($routeProvider) {
 	$routeProvider.when('/tasks/sync/:wbs', {
 		templateUrl:'tasks/tasks.tpl.html',
 		controller:'TasksController'
 	});
 }])
-.controller('TasksController', ['$scope', '$location', '$routeParams', 'Tasks', function ($scope, $location, $routeParams, Tasks) {
+.factory('taskSortable', function(){
+	var taskSortable = {
+		sortableOptions : {
+			cursor: "intersect",
+			tolerance: 'pointer',
+			revert: 'invalid',
+			placeholder: 'active',
+		 	forceHelperSize: true,
+			axis: 'y',
+			helper : getSortHelper, // postion:absolute 상태에서 diplay:table-cell 정상 표시 안되는 문제 해결
+			start  : onSortStarted,
+			update : onSortUpdated
+		}
+	};
+	function getSortHelper(event) {
+		var element = 	'<div class="table-responsive">' +
+							'<table  class="table"><tbody></tbody></table>' +
+						'</div>';
+		return angular.element(element)
+					.find('tbody').append(angular.element(event.target).closest('tr').clone()).end().appendTo('div.container');
+	}
+	function onSortStarted(event, ui) {
+		var test01 = angular.element('.ui-sortable').sortable('toArray'),
+			test02 = angular.element('.ui-sortable').sortable('serialize');
+
+		console.log(test01 + '/' + test02);
+	}
+	function onSortUpdated(event, ui) {
+		var test01 = angular.element('.ui-sortable').sortable('toArray'),
+			test02 = angular.element('.ui-sortable').sortable('serialize');
+
+		console.log(test01 + '/' + test02);
+	}
+
+	return taskSortable;
+})
+.controller('TasksController', ['$scope', '$location', '$routeParams', 'Tasks', 'taskSortable', function ($scope, $location, $routeParams, Tasks, taskSortable) {
 	$scope.currentWbs = $routeParams.wbs;
 	$scope.currentPath = $location.path().replace($routeParams.wbs, '');
+	$scope.sortableOptions = taskSortable.sortableOptions;
 
 	$scope.$watch('currentWbs', function() {
 		$scope.getTask();
